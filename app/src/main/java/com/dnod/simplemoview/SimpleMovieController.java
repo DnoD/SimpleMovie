5b98104aeb7d461f272f309f55b5caf19005c4a0
Dimon <dimon.zakrasin@gmail.com>
2017-03-03 11:26:55 +0200

    Initial develop commit

---
+++ app/src/main/java/com/dnod/simplemoview/SimpleMovieController.java
@@ -0,0 +1,6 @@
+package com.dnod.simplemoview;
+
+import android.app.Application;
+
+public class SimpleMovieController extends Application {
+}
--- app/src/main/java/com/dnod/simplemoview/ui/MainActivity.java
+++ app/src/main/java/com/dnod/simplemoview/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.dnod.simplemoview;
+package com.dnod.simplemoview.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.dnod.simplemoview.R;
 
 public class MainActivity extends AppCompatActivity {
 
